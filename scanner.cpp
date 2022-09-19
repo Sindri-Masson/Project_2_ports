6b788d598c2322db9ad97e77bd258c579e6783b8
arnormoncada <arnormoncada@gmail.com>
2022-09-19 23:18:13 +0000

    problem one finished

--- scanner.cpp
+++ scanner.cpp
@@ -11,7 +11,7 @@ typedef unsigned int ADDRESS_SIZE;
 
 
 //Take a reference to a socket address and connect it to the given ip and port
-void make_socket_address(sockaddr *address, int port, std::string ip_address)
+void make_socket_address(sockaddr_in *address, int port, std::string ip_address)
 {
     sockaddr_in *socket_address = (sockaddr_in *)address;
     socket_address->sin_family = AF_INET;
@@ -30,18 +30,21 @@ void send_to_server(int socket, std::string message)
 int scan_ports(char *ip, int low, int high)
 {
     int socket_fd;
-    struct sockaddr address;
+    struct sockaddr_in address;
     int port;
     int result;
     int i;
     int count = 0;
     std::vector<int> open_ports;
-
+    std:: string sentmsg = "Hello World";
+    char buffer[4096];
     
+    // Create a socket
+
+
     for (i = low; i <= high; i++)
     {
-        // Create a socket
-        socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+        socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
         if (socket_fd < 0)
         {
             perror("Error creating socket\n");
@@ -50,26 +53,33 @@ int scan_ports(char *ip, int low, int high)
         // Set the address
         make_socket_address(&address, i, ip);
 
-        // Scan the port
-        
-        if (connect(socket_fd, &address, sizeof(address)) < 0)
+        //set timeout so recv doesnt halt forever on non responding ports
+        struct timeval read_timeout;
+        read_timeout.tv_sec = 0;
+        read_timeout.tv_usec = 30000;
+        setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof(read_timeout));
+
+
+        sendto(socket_fd,"HelloWorld",sizeof("HelloWorld") - 1,0,(struct sockaddr *)&address,sizeof(address));
+
+        socklen_t len = sizeof(address);
+        int nread = recvfrom(socket_fd,buffer,sizeof(buffer),0,(struct sockaddr *)&address,&len);       
+        if(nread > 0)
         {
-            std::cout << "Port " << i << " is closed" << std::endl;
+            std:: cout << "from " << inet_ntoa(address.sin_addr) << " port " << ntohs(address.sin_port) << " : " << buffer << std:: endl;
         }
-        else
-        {
-            open_ports.push_back(i);
-            count++;
-        }
+        memset(buffer, 0, sizeof(buffer));
+
+
         close(socket_fd);
     }
 
     // Print the open ports
-    std::cout << "Open ports: " << count << std::endl;
-    for (i = 0; i < open_ports.size(); i++)
-    {
-        std::cout << open_ports[i] << std::endl;
-    }
+    // std::cout << "Open ports: " << count << std::endl;
+    // for (i = 0; i < open_ports.size(); i++)
+    // {
+    //     std::cout << open_ports[i] << std::endl;
+    // }
 
     return 0;
 }
